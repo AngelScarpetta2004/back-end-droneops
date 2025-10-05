a23205a9e45226cbe14618c93417a317a849d25a
Angel Daniel Scarpetta Ruiz <114451660+AngelScarpetta2004@users.noreply.github.com>
2025-10-04 20:13:47 -0500

    fix to settings.py

--- droneops/settings.py
+++ droneops/settings.py
@@ -112,7 +112,9 @@ WSGI_APPLICATION = 'droneops.wsgi.application'
 # -----------------------------
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
